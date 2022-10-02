c86e8b30755deaf223799129fa371f19c7a0fad4
abhinavginti <abhinavginti@gmail.com>
2022-10-02 12:24:15 +0530

    single player mode completed

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let gameOver = false
 
 const boxes = document.querySelectorAll('.__box')
 const msgbox = document.querySelector('#msg-box')
+// const boxContainer = document.querySelector('.__box-container')
+const line = document.querySelector('#__line')
 
 const resetGame = () => {
     boxes.forEach(box => {
@@ -11,15 +13,20 @@ const resetGame = () => {
         gameOver = false;
         turn = 'X'
         msgbox.innerHTML = ''
+        line.classList.remove('active')
+        line.style.opacity = 0;
+        line.style.transform = `translateX(-50%)`;
+        line.style.left = '50%'
+        // line.style.width = 'calc(10vmax * 3)'
     })
 }
 
 boxes.forEach(box => {
     box.addEventListener('click', () => {
-        if (!gameOver) {
+        if (!gameOver && box.innerText === '') {
             box.innerText = turn;
             checkWin()
-            toggleTurn();
+            // toggleTurn();
         }
     })
 })
@@ -34,22 +41,46 @@ const equals3 = (a, b, c) => {
 
 const checkWin = () => {
     const winConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        [0, 1, 2, 0, 6, 0],
+        [3, 4, 5, 0, 2, 0],
+        [6, 7, 8, 0, 1.2, 0],
+        [0, 3, 6, 3, 2, 90],
+        [1, 4, 7, 0, 2, 90],
+        [2, 5, 8, -3, 2, 90],
+        [0, 4, 8, 0, 2, 45],
+        [2, 4, 6, 0, 2, 135]
     ]
+
+    let winner = null;
 
     winConditions.forEach(win => {
         if (equals3(boxes[win[0]], boxes[win[1]], boxes[win[2]])) {
-            msgbox.innerText = `Player ${turn} Wins`
+            msgbox.innerText = `Player ${boxes[win[0]].innerText} Wins`
             gameOver = true
+            winner = boxes[win[0]].innerText
+            line.style.opacity = 1;
+            if (win[3]) {
+                // line.style.left = `calc(50% - ${boxContainer.clientHeight / win[3]}px)`
+                line.style.left = `calc(50% - (30vmax / ${win[3]}))`
+            }
+            // line.style.transform = `translateX(-50%) translateY(${boxContainer.clientHeight / win[4]}px) rotate(${win[5]}deg)`;
+            line.style.transform = `translateX(-50%) translateY(calc(30vmax / ${win[4]})) rotate(${win[5]}deg)`;
+            line.classList.add('active')
         }
     })
+    if (winner !== null) return winner;
+
+    let ifTie = true;
+
+    boxes.forEach(box => {
+        if (box.innerText === '') {
+            ifTie = false;
+        }
+    })
+    if (ifTie) {
+        msgbox.innerText = `It is a Draw`
+        return 'tie'
+    }
 }
 
 const toggleTurn = () => {
